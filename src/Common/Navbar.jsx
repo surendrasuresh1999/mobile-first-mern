b2ddfeabd5afad844952b98a38feb8bb9fd08427
surendrasuresh1999 <k.surendraparla1999@gmail.com>
2024-06-10 15:22:13 +0530

    fix://nav bar issues fixed

--- src/Common/Navbar.jsx
+++ src/Common/Navbar.jsx
@@ -34,6 +34,9 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
+  const userInformation = JSON.parse(
+    localStorage.getItem("mernTraineeUserInfo")
+  );
   return (
     <>
       {/* When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars */}
@@ -50,19 +53,17 @@ export default function Navbar() {
           <>
             <div className="shadow">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8  relative flex justify-between lg:gap-8 xl:grid xl:grid-cols-12">
-                <div className="flex md:absolute md:inset-y-0 md:left-0 lg:static xl:col-span-2">
+                <div className="flex md:absolute md:inset-y-0 lg:static xl:col-span-2">
                   <div className="flex flex-shrink-0 items-center">
-                    <a href="#">
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                      />
-                    </a>
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      alt="Your Company"
+                    />
                   </div>
                 </div>
                 <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-8">
-                  <div className="flex items-center px-6 py-4 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0">
+                  <div className="flex items-center px-4 py-6 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0">
                     <div className="w-full">
                       <div className="relative text-center">
                         <h1 className="text-indigo-800 text-xs sm:text-3xl font-bold">
@@ -72,136 +73,15 @@ export default function Navbar() {
                     </div>
                   </div>
                 </div>
-                <div className="flex items-center md:absolute md:inset-y-0 md:right-0 lg:hidden">
-                  {/* Mobile menu button */}
-                  <PopoverButton className="relative -mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Open menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </PopoverButton>
-                </div>
-                <div className="hidden lg:flex lg:items-center lg:justify-end xl:col-span-2">
-                  {/* <button
-                    type="button"
-                    className="relative ml-5 flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button> */}
-
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-5 flex-shrink-0">
-                    <div>
-                      <MenuButton className="relative flex rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src={user.imageUrl}
-                          alt=""
-                        />
-                      </MenuButton>
-                    </div>
-                    <Transition
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {userNavigation.map((item) => (
-                          <MenuItem key={item.name}>
-                            {({ focus }) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  focus ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </MenuItem>
-                        ))}
-                      </MenuItems>
-                    </Transition>
-                  </Menu>
-
-                  {/* <a
-                    href="#"
-                    className="ml-6 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    New Project
-                  </a> */}
+                <div className="flex items-center justify-end xl:col-span-2">
+                  <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-500">
+                    <span className="font-medium leading-none text-white">
+                      {userInformation.name.slice(0, 2).toUpperCase()}
+                    </span>
+                  </span>
                 </div>
               </div>
             </div>
-
-            <PopoverPanel as="nav" className="lg:hidden" aria-label="Global">
-              <div className="mx-auto max-w-3xl space-y-1 px-2 pb-3 pt-2 sm:px-4">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-100 text-gray-900"
-                        : "hover:bg-gray-50",
-                      "block rounded-md px-3 py-2 text-base font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="border-t border-gray-200 pb-3 pt-4">
-                <div className="mx-auto flex max-w-3xl items-center px-4 sm:px-6">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={user.imageUrl}
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium text-gray-800">
-                      {user.name}
-                    </div>
-                    <div className="text-sm font-medium text-gray-500">
-                      {user.email}
-                    </div>
-                  </div>
-                  <button
-                    type="button"
-                    className="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="mx-auto mt-3 max-w-3xl space-y-1 px-2 sm:px-4">
-                  {userNavigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </PopoverPanel>
           </>
         )}
       </Popover>
